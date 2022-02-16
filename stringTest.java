2c43908f4d487776267519b2bac208e5251b661d
julianmpineda <82466536+julianmpineda@users.noreply.github.com>
2022-02-16 17:53:47 -0500

    Logic change
    changed to while loop, added different length return

--- stringTest.java
+++ stringTest.java
@@ -1,15 +1,18 @@
 public static int stringTest(String one, String two) {
 
-	//returns first different index, or -1 if they match fully.
+	//returns first different index, or -1 if they match fully, -2 if they are different lengths.
 	
-	boolean flag = true;
-	int count = 0;
-	for (int i = 0; i < one.length() && flag; i++) {
-		flag = two.charAt(i) == one.charAt(i);
-		count = i;
+	if (one.length() != two.length()) {
+		return -2;
 	}
 	
-	if (!flag) {
+	int count = 0;
+	
+	while (count < one.length && two.charAt(count) == one.charAt(count)) {
+		count++;
+	}
+	
+	if (count != one.length()) {
 		return count;
 	} else {
 		return -1;
