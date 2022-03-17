a602fd00ba561d24d016d28538ccb63718d82985
julianmpineda <82466536+julianmpineda@users.noreply.github.com>
2022-03-17 14:56:01 -0400

    rewrite Day4.java
    Huge rewrite of Day 4. Much faster and less space. Fully contained in 1 method now.
    
    - Reads from file only once and creates array and list<int[][]> - down from opening scanner to get a single board.
    - Reads from proper input, not a cheated input with only boards.
    - Combines part 1 and 2 based on above structures.
    - New logic change using new 6x6 array with 5th index row/columns storing if numbers were found.
    
    To look into:
    - Reduce number of loops? currently a lot of nested loops
    - combine final loops that count winning sum for boards - store in unused [5][5] cell?

--- Day4.java
+++ Day4.java
@@ -1,195 +1,106 @@
-	public static int day4() {
-		int[] balls = new int[] { 1, 76, 38, 96, 62, 41, 27, 33, 4, 2, 94, 15, 89, 25, 66, 14, 30, 0, 71, 21, 48, 44,
-				87, 73, 60, 50, 77, 45, 29, 18, 5, 99, 65, 16, 93, 95, 37, 3, 52, 32, 46, 80, 98, 63, 92, 24, 35, 55,
-				12, 81, 51, 17, 70, 78, 61, 91, 54, 8, 72, 40, 74, 68, 75, 67, 39, 64, 10, 53, 9, 31, 6, 7, 47, 42, 90,
-				20, 19, 36, 22, 43, 58, 28, 79, 86, 57, 49, 83, 84, 97, 11, 85, 26, 69, 23, 59, 82, 88, 34, 56, 13 };
+public static void day4() {
+    String[] ballList = new String[] {};
+    List<int[][]> boardList = new ArrayList<int[][]>();
+	List<Integer> winList = new ArrayList<Integer>();
 
-		boolean win = false;
-		boolean[][] callMap = new boolean[5][5];
-		int[][] bingoBoard = new int[5][5];
-
-		int ballNum = 4, start = 0, count = 1;
-
-		while (!win) {
-
-			start = 0;
-			count = 1;
-			ballNum++;
-
-			while (start < 600 && !win) {
-
-				bingoBoard = getBoard(start);
-				int[] ballsCalled = Arrays.copyOfRange(balls, 0, ballNum);
-
-				for (int i = 0; i < 5 && !win; i++) {
-					win = checkWin(bingoBoard[i], ballsCalled);
-					if (win) {
-						System.out.print("Win on board " + count + " column " + (i + 1));
-						System.out.print(" on ball " + ballNum);
-						System.out.println();
-					}
-				}
-
-				if (!win) {
-					for (int i = 0; i < 5 && !win; i++) {
-						int[] line = new int[5];
-						for (int j = 0; j < 5; j++) {
-							line[j] = bingoBoard[j][i];
-						}
-						win = checkWin(line, ballsCalled);
-						if (win) {
-							System.out.print("Win on board " + count + " column " + (i + 1));
-							System.out.print(" on ball " + ballNum);
-							System.out.println();
-						}
-					}
-				}
-				start += 6;
-				count++;
-			}
-		}
-
-		int sum = 0;
-
-		for (int i = 0; i < 5; i++) {
+    try {
+        File myObj = new File("advent4.txt");
+		Scanner myReader = new Scanner(myObj);
+        
+		ballList = myReader.nextLine().split(",");
+		myReader.nextLine();
+        
+	while (myReader.hasNextLine()) {
+        int[][] board = new int[6][6];
+        for (int i = 0; i < 5; i++) {
+			board[i][5] = 0;
+            board[5][i] = 0;
 			for (int j = 0; j < 5; j++) {
-				for (int k = 0; k < ballNum; k++) {
-					if (bingoBoard[i][j] == balls[k]) {
-						callMap[i][j] = true;
-					}
-				}
-			}
-		}
+                board[i][j] = myReader.nextInt();
+            }
+        }
+			boardList.add(board);
+			myReader.nextLine();
+        	}
 
-		for (int i = 0; i < 5; i++) {
-			for (int j = 0; j < 5; j++) {
-				if (!callMap[i][j]) {
-					sum += bingoBoard[i][j];
-				}
-			}
-		}
-
-		return sum * balls[ballNum - 1];
-	}
-
-	public static int day4pt2() {
-		int[] balls = new int[] { 1, 76, 38, 96, 62, 41, 27, 33, 4, 2, 94, 15, 89, 25, 66, 14, 30, 0, 71, 21, 48, 44,
-				87, 73, 60, 50, 77, 45, 29, 18, 5, 99, 65, 16, 93, 95, 37, 3, 52, 32, 46, 80, 98, 63, 92, 24, 35, 55,
-				12, 81, 51, 17, 70, 78, 61, 91, 54, 8, 72, 40, 74, 68, 75, 67, 39, 64, 10, 53, 9, 31, 6, 7, 47, 42, 90,
-				20, 19, 36, 22, 43, 58, 28, 79, 86, 57, 49, 83, 84, 97, 11, 85, 26, 69, 23, 59, 82, 88, 34, 56, 13 };
-
-		boolean win = false;
-		boolean[][] callMap = new boolean[5][5];
-		int[][] bingoBoard = new int[5][5];
-		boolean[] boardWin = new boolean[100];
-
-		int ballNum = 4, start = 0, count = 1;
-
-		while (ballNum < 100) {
-
-			start = 0;
-			count = 1;
-			ballNum++;
-
-			while (start < 600) {
-				if (!boardWin[count - 1]) {
-					// System.out.println("Board " + count + " being tested.");
-					bingoBoard = getBoard(start);
-					int[] ballsCalled = Arrays.copyOfRange(balls, 0, ballNum);
-
-					for (int i = 0; i < 5 && !boardWin[count - 1]; i++) {
-						win = checkWin(bingoBoard[i], ballsCalled);
-						if (win) {
-							boardWin[count - 1] = true;
-						}
-					}
-
-					if (!win) {
-						for (int i = 0; i < 5 && !boardWin[count - 1]; i++) {
-							int[] line = new int[5];
-							for (int j = 0; j < 5; j++) {
-								line[j] = bingoBoard[j][i];
-							}
-							win = checkWin(line, ballsCalled);
-							if (win) {
-								boardWin[count - 1] = true;
-							}
-						}
-					}
-				}
-				start += 6;
-				count++;
-			}
-		}
-		int sum = 0;
-
-		bingoBoard = getBoard(72 * 6);
-		ballNum = 86;
-
-		for (int i = 0; i < 5; i++) {
-			for (int j = 0; j < 5; j++) {
-				for (int k = 0; k < ballNum; k++) {
-					if (bingoBoard[i][j] == balls[k]) {
-						callMap[i][j] = true;
-					}
-				}
-			}
-		}
-		for (int i = 0; i < 5; i++) {
-			for (int j = 0; j < 5; j++) {
-				if (!callMap[i][j]) {
-					sum += bingoBoard[i][j];
-				}
-			}
-		}
-		return sum * balls[ballNum - 1];
-	}
-
-	public static int[][] getBoard(int start) {
-
-		String[][] bingoBoard = new String[5][5];
-		int[][] board = new int[5][5];
-
-		int row = 0;
-
-		try {
-			File myObj = new File("advent4sf.txt");
-			Scanner myReader = new Scanner(myObj);
-
-			for (int i = 0; i < start; i++) {
-				myReader.nextLine();
-			}
-			while (row < 5) {
-				String line = myReader.nextLine();
-				bingoBoard[row] = line.split("\\s+");
-				row++;
-			}
 			myReader.close();
 		} catch (FileNotFoundException e) {
 			System.out.println("An error occurred.");
 			e.printStackTrace();
 		}
-		for (int i = 0; i < 5; i++) {
-			for (int j = 0; j < 5; j++) {
-				board[i][j] = Integer.parseInt(bingoBoard[i][j]);
-			}
-		}
-		return board;
-	}
 
-	public static boolean checkWin(int[] line, int[] balls) {
+		// i = ball, j = board
+		int firstWin = Integer.MAX_VALUE, lastWin = Integer.MIN_VALUE;
 
-		int count = 0;
+		for (int i = 0; i < ballList.length; i++) {
+			int ball = Integer.parseInt(ballList[i]);
+			for (int j = 0; j < boardList.size(); j++) {
+				if (!winList.contains(j)) {
+					int[][] board = boardList.get(j);
+					boolean found = false;
 
-		for (int i = 0; i < 5; i++) {
-			for (int j = 0; j < balls.length; j++) {
-				if (line[i] == balls[j]) {
-					count++;
+					for (int q = 0; q < 5 && !found; q++) {
+						for (int r = 0; r < 5; r++) {
+							if (board[q][r] == ball) {
+								board[q][5]++;
+								board[5][r]++;
+
+								if (board[q][5] == 5 || board[5][r] == 5) {
+									winList.add(j);
+									firstWin = Math.min(firstWin, i);
+									lastWin = Math.max(lastWin, i);
+								}
+								found = true;
+							}
+						}
+					}
 				}
 			}
 		}
-		if (count == 5) {
-			return true;
+
+		int[][] board = boardList.get(winList.get(0));
+		int count = 0, boardSum = 0;
+
+		while (count <= firstWin) {
+			for (int i = 0; i < 5; i++) {
+				for (int j = 0; j < 5; j++) {
+					if (board[i][j] == Integer.parseInt(ballList[count])) {
+						board[i][j] = 0;
+					}
+				}
+			}
+			count++;
 		}
-		return false;
-	}
+
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 5; j++) {
+				boardSum += board[i][j];
+			}
+		}
+		
+		System.out.println("Part 1: " + boardSum * Integer.parseInt(ballList[firstWin]));
+
+		board = boardList.get(winList.get(winList.size() - 1));
+		count = 0;
+		boardSum = 0;
+
+		while (count <= lastWin) {
+			for (int i = 0; i < 5; i++) {
+				for (int j = 0; j < 5; j++) {
+					if (board[i][j] == Integer.parseInt(ballList[count])) {
+						board[i][j] = 0;
+					}
+				}
+			}
+			count++;
+		}
+
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 5; j++) {
+				boardSum += board[i][j];
+			}
+		}
+
+	
+		System.out.println("Part 2: " + boardSum * Integer.parseInt(ballList[lastWin]));
+    }
