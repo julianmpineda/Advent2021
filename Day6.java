2d6885e4c688ce4ff722476b545cd81ab0ccb5b3
julianmpineda <82466536+julianmpineda@users.noreply.github.com>
2022-03-21 11:12:28 -0400

    Condense Day6.java
    Quick rewrite, same functionality.
    
    -Reads from file instead of predefined list to standardize for different inputs
    -Reduced eight placeholder variables to only one placeholder variable when shifting fish between days. 
    -Reuse "sum" for placeholder and final return to save even more space.

--- Day6.java
+++ Day6.java
@@ -1,52 +1,44 @@
-	public static long day6(int days) {
+public static long day6(int days) {
 
-		ArrayList<Integer> fish = new ArrayList<Integer>(Arrays.asList(5, 1, 1, 4, 1, 1, 4, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-				1, 1, 4, 2, 1, 1, 1, 3, 5, 1, 1, 1, 5, 4, 1, 1, 1, 2, 2, 1, 1, 1, 2, 1, 1, 1, 2, 5, 2, 1, 2, 2, 3, 1, 1,
-				1, 1, 1, 1, 1, 1, 5, 1, 1, 4, 1, 1, 1, 5, 4, 1, 1, 3, 3, 2, 1, 1, 1, 5, 1, 1, 4, 1, 1, 5, 1, 1, 5, 1, 2,
-				3, 1, 5, 1, 3, 2, 1, 3, 1, 1, 4, 1, 1, 1, 1, 2, 1, 2, 1, 1, 2, 1, 1, 1, 4, 4, 1, 5, 1, 1, 3, 5, 1, 1, 5,
-				1, 4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 3, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 5, 1, 1, 1, 1,
-				4, 1, 1, 1, 1, 4, 1, 1, 1, 1, 3, 1, 2, 1, 2, 1, 3, 1, 3, 4, 1, 1, 1, 1, 1, 1, 1, 5, 1, 1, 1, 1, 1, 1, 1,
-				1, 4, 1, 1, 2, 2, 1, 2, 4, 1, 1, 3, 1, 1, 1, 5, 1, 3, 1, 1, 1, 5, 5, 1, 1, 1, 1, 2, 3, 4, 1, 1, 1, 1, 1,
-				1, 1, 1, 1, 1, 1, 1, 5, 1, 4, 3, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-				3, 3, 1, 2, 2, 1, 4, 1, 5, 1, 5, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 5, 1, 1, 1, 4,
-				3, 1, 1, 4));
-
-		long[] fishcount = new long[9];
-		long zero = 0, one = 0, two = 0, three = 0, four = 0, five = 0, six = 0, seven = 0, eight = 0;
-
-		long sum = 0;
-
-		for (int num : fish) {
-			fishcount[num]++;
+	List<Integer> fish = new ArrayList<Integer>();
+	long[] fishcount = new long[9];
+	long sum = 0L;
+	
+	try {
+		File myObj = new File("advent6.txt");
+		Scanner myReader = new Scanner(myObj).useDelimiter("\\D");
+		while (myReader.hasNext()) {
+			fish.add(myReader.nextInt());
 		}
-
-		for (int i = 0; i < days; i++) {
-			zero = fishcount[0];
-			one = fishcount[1];
-			two = fishcount[2];
-			three = fishcount[3];
-			four = fishcount[4];
-			five = fishcount[5];
-			six = fishcount[6];
-			seven = fishcount[7];
-			eight = fishcount[8];
-
-			fishcount[0] = one;
-			fishcount[1] = two;
-			fishcount[2] = three;
-			fishcount[3] = four;
-			fishcount[4] = five;
-			fishcount[5] = six;
-			fishcount[6] = seven + zero;
-			fishcount[7] = eight;
-			fishcount[8] = zero;
-
-		}
-
-		for (long num : fishcount) {
-			sum += num;
-		}
-
-		return sum;
-
+		myReader.close();
+	} catch (FileNotFoundException e) {
+		System.out.println("An error occurred.");
+		e.printStackTrace();
 	}
+
+	for (int num : fish) {
+		fishcount[num]++;
+	}
+
+	for (int i = 0; i < days; i++) {
+		sum = fishcount[0];
+		fishcount[0] = fishcount[1];
+		fishcount[1] = fishcount[2];
+		fishcount[2] = fishcount[3];
+		fishcount[3] = fishcount[4];
+		fishcount[4] = fishcount[5];
+		fishcount[5] = fishcount[6];
+		fishcount[6] = fishcount[7] + sum;
+		fishcount[7] = fishcount[8];;
+		fishcount[8] = sum;
+	}
+
+    sum = 0;
+    
+	for (long num : fishcount) {
+        sum += num;
+	}
+
+	return sum;
+
+}
